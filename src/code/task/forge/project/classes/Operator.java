3086b3fe630be84cf237a97956088b8d4560218a
André Marques <andrepestanamarques@gmail.com>
2021-03-24 23:41:15 +0000

    Operator attributes created
    add attributes operator

--- src/code/task/forge/project/classes/Operator.java
+++ src/code/task/forge/project/classes/Operator.java
@@ -1,4 +1,25 @@
 package code.task.forge.project.classes;
+import java.lang.reflect.Array;
 
+/**
+ * Class Operador
+ */
 public class Operator {
+    /**
+     *Codigo do operador
+     */
+    private String code;
+    /**
+     *Nome do operador
+     */
+    private String name;
+    /**
+     *Array do Horario
+     */
+    private Array schedule;
+    /**
+     *Estado do operador Ativo ou Inativo
+     */
+    private boolean state;
+
 }
