e61e19234a8edc78a9985c91bf77e1ca7d499ebb
João Gomes <joaopfg.2002@gmail.com>
2021-03-24 23:43:23 +0000

    add Part Class atributes

--- src/code/task/forge/project/classes/Part.java
+++ src/code/task/forge/project/classes/Part.java
@@ -1,4 +1,51 @@
 package code.task.forge.project.classes;
+import java.util.*;
 
 public class Part {
+
+    /**
+     * Referencia
+     */
+    private String reference;
+
+    /**
+     * Designação
+     */
+    private String designation;
+
+    /**
+     * Designação comercial
+     */
+    private String commercialDesignation;
+
+    /**
+     * Quantidade Lote
+     */
+    private int batchSize;
+
+    /**
+     * Unidade
+     */
+    private String unit;
+
+    /**
+     * Versão
+     */
+    private String version;
+
+    /**
+     * Estado
+     */
+    private boolean status;
+
+    /**
+     * Operações
+     */
+    private ArrayList<Operation> operations;
+
+    /**
+     * Componentes
+     */
+    private ArrayList<Component> components;
+
 }
