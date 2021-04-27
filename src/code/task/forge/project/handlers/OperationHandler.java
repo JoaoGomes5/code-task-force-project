6f22cc5497111382464af7e465e0cf083cf8861d
Ricardo <1200374@isep.ipp.pt>
2021-04-27 23:01:02 +0100

    Create part Operation  functions

---
+++ src/code/task/forge/project/handlers/OperationHandler.java
@@ -0,0 +1,46 @@
+package code.task.forge.project.handlers;
+import code.task.forge.project.classes.Operation;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class OperationHandler {
+
+    ArrayList<Operation> operations = new ArrayList();
+    
+    
+    static BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
+/**
+ * 
+ * @throws IOException 
+ */ 
+    public static void createOperation() throws IOException {
+        System.out.println("Intruduza a ordem da operação!");
+        int operationOrder = reader.read();
+        System.out.println("Introduza a identificação da operação!");
+        String operationName = reader.readLine();
+        System.out.println("Introduza a quantidade de operadores necessários para a operação!");
+        int operationOperatorsNeeded = reader.read();
+        System.out.println("Introduza o tempo em segundos da operação!");
+        int operationExecutionTime = reader.read();
+        System.out.println("Introduza quantidade produzida por hora na operação! ");
+        int operationQuantityPerHour = reader.read();
+        System.out.println("Introduza as instruções técnicas da operação!");
+        String operationInstructions = reader.readLine();
+        
+        
+
+        Operation operation = new Operation (operationOrder, operationName,createMachine, operationOperatorsNeeded, operationExecutionTime, operationQuantityPerHour, operationInstructions);
+
+        operations.add(operation);
+        
+        
+        
+    }
+
+}
