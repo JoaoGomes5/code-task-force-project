ac7eeb7ab1f14991580ee202ee189e133af1873f
Ricardo <1200374@isep.ipp.pt>
2021-04-27 23:02:27 +0100

    Create part Operation functions

--- src/code/task/forge/project/handlers/OperationHandler.java
+++ src/code/task/forge/project/handlers/OperationHandler.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class OperationHandler {
 
-    ArrayList<Operation> operations = new ArrayList();
+    ArrayList<Operation> operations = new ArrayList<Operation>();
     
     
     static BufferedReader reader =
