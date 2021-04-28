592ef390c7538265c1244bd1cdf49eb394003206
Ricardo <1200374@isep.ipp.pt>
2021-04-28 21:17:40 +0100

    Fix return class operation

--- src/code/task/forge/project/handlers/OperationHandler.java
+++ src/code/task/forge/project/handlers/OperationHandler.java
@@ -1,30 +1,32 @@
 package code.task.forge.project.handlers;
+
+import code.task.forge.project.classes.Machine;
 import code.task.forge.project.classes.Operation;
 import com.sun.javafx.scene.control.skin.VirtualFlow;
-
-
+import code.task.forge.project.classes.Component;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-
 public class OperationHandler {
 
     ArrayList<Operation> operations = new ArrayList<Operation>();
-    
-    
-    static BufferedReader reader =
-            new BufferedReader(new InputStreamReader(System.in));
-/**
- * 
- * @throws IOException 
- */ 
-    public static void createOperation() throws IOException {
+
+    static BufferedReader reader
+            = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     *
+     * @throws IOException
+     */
+    public Operation createOperation() throws IOException {
         System.out.println("Intruduza a ordem da operação!");
         int operationOrder = reader.read();
         System.out.println("Introduza a identificação da operação!");
         String operationName = reader.readLine();
+        Machine machine = MachineHandler.CreateMachine();
+
         System.out.println("Introduza a quantidade de operadores necessários para a operação!");
         int operationOperatorsNeeded = reader.read();
         System.out.println("Introduza o tempo em segundos da operação!");
@@ -33,15 +35,14 @@ public class OperationHandler {
         int operationQuantityPerHour = reader.read();
         System.out.println("Introduza as instruções técnicas da operação!");
         String operationInstructions = reader.readLine();
-        
-        
+        Component component = ComponentHandler.createComponent();
 
-        Operation operation = new Operation (operationOrder, operationName,createMachine, operationOperatorsNeeded, operationExecutionTime, operationQuantityPerHour, operationInstructions);
+        Operation operation = new Operation(operationOrder, operationName, machine, operationOperatorsNeeded, operationExecutionTime, operationQuantityPerHour, operationInstructions, component);
 
         operations.add(operation);
-        
-        
-        
+
+        return operation;
+
     }
 
 }
