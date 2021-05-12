e7677f8c8a9fe0136bd1967fb80e6572819c2661
Gustavo Reis <1200360@isep.ipp.pt>
2021-05-12 22:29:08 +0100

    Fix javadocs

--- src/code/task/forge/project/classes/Line.java
+++ src/code/task/forge/project/classes/Line.java
@@ -46,6 +46,18 @@ public class Line {
     public Line(){
     }
 
+    /**
+     *
+     * Método que permite criar Linha
+     *
+     * @param reference Linha de Referência do Produto
+     * @param description Linha de Descrição do Produto
+     * @param version Linha de Versão do Produto
+     * @param quantity Linha de Quantidade do Produto
+     * @param unit Linha de Unidade do Produto
+     * @param unitPrice Linha de Preço Unitário do Produto
+     * @param valorTotal Linha de Valor Total
+     */
     public Line (String reference, String description, String version, int quantity, String unit, String unitPrice, String valorTotal) {
         this.setReference(reference);
         this.setDescription(description);
