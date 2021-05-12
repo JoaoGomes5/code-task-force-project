6fb1ffc75354177b6ae8d1e141a1162e17108960
Joao <joaopfg,2002@gmail.com>
2021-05-12 21:24:36 +0100

    Fix Part Handler

--- src/code/task/forge/project/handlers/PartHandler.java
+++ src/code/task/forge/project/handlers/PartHandler.java
@@ -2,12 +2,9 @@ package code.task.forge.project.handlers;
 
 
 import code.task.forge.project.classes.Part;
-import code.task.forge.project.classes.Operation;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class PartHandler {
@@ -16,63 +13,7 @@ public class PartHandler {
 
 
     public static Part createPart() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-
-        System.out.println("Introduza a referência");
-            String reference = reader.readLine();
-        System.out.println("Introduza a designação");
-            String designation = reader.readLine();
-        System.out.println("Introduza a designação comercial");
-            String commercialDesignation = reader.readLine();
-        System.out.println("Introduza a designação comercial");
-            int batchSize = reader.read();
-        System.out.println("Introduza a unidade");
-            String unit = reader.readLine();
-        System.out.println("Introduza a versão");
-            String version = reader.readLine();
-        System.out.println("Introduza o estado");
-        System.out.println("1- Ativo /n2- Inativo");
-        int option = reader.read();
-
-        boolean state;
-        switch (option) {
-            case 1 : 
-                state = true;
-                break;
-            case 2 : 
-                state = false;
-                break;
-            default:
-                throw new IllegalStateException("Valor invalido: " + option);
-        }
-        
-        System.out.println("Introduza o numero de operações");
-        int operationsQuantity = reader.read();
-
-        ArrayList<Operation> operations = new ArrayList<Operation>();
-
-        for(int i = 0; i < operationsQuantity; i++) {
-            System.out.println("Operação nº " + i);
-            Operation operation = OperationHandler.createOperation();
-            operations.add(operation);
-        }
-
-
-        Part part = new Part(
-                reference,
-                designation,
-                commercialDesignation,
-                batchSize,
-                unit,
-                version,
-                state,
-                operations
-        );
        
-        parts.add(part);
-        
-        return part;
 
     }
     
