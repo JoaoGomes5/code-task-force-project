211cb3ac3d57f58050cf8172c50ee604d130529d
André Marques <andrepestanamarques@gmail.com>
2021-05-26 21:51:37 +0100

    ControllersUpdate

--- src/code/task/forge/project/Controllers/ClientsManagerController.java
+++ src/code/task/forge/project/Controllers/ClientsManagerController.java
@@ -7,7 +7,11 @@ package code.task.forge.project.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 /**
  * FXML Controller class
@@ -16,6 +20,15 @@ import javafx.fxml.Initializable;
  */
 public class ClientsManagerController implements Initializable {
 
+    @FXML
+    private ListView<?> listViewArticles;
+    @FXML
+    private Button btnEditClient;
+    @FXML
+    private Button btnCreateClient;
+    @FXML
+    private Button btnReturn;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +36,17 @@ public class ClientsManagerController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void goToEditClient(ActionEvent event) {
+    }
+
+    @FXML
+    private void goToCreateClient(ActionEvent event) {
+    }
+
+    @FXML
+    private void goReturn(ActionEvent event) {
+    }
     
 }
