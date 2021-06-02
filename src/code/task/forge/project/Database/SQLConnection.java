8dde18634e9ebc1db1c0e8e416715660bccd0b94
Cristiano Martins <1200351@isep.ipp.pt>
2021-06-02 21:55:18 +0100

    Diagrams

--- src/code/task/forge/project/Database/SQLConnection.java
+++ src/code/task/forge/project/Database/SQLConnection.java
@@ -25,7 +25,7 @@ public class SQLConnection {
 
         Connection con = getDatabaseConnection();
         Statement st = con.createStatement();
-        String query = "Select * from teste";
+        String query = "Select * from component";
         ResultSet rs = st.executeQuery(query);
         while(rs.next()){
 
