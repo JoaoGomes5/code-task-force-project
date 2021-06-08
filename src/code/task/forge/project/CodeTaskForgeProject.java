ab943ebe1a041a3e6eabd37a7eb4b0b6fdf9427f
André Marques <andrepestanamarques@gmail.com>
2021-06-08 22:25:05 +0100

    Commit FMXL Complete

--- src/code/task/forge/project/CodeTaskForgeProject.java
+++ src/code/task/forge/project/CodeTaskForgeProject.java
@@ -1,28 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package code.task.forge.project;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import code.task.forge.project.Controllers.DatasheetController;
-
-import code.task.forge.project.Database.SQLConnection;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 
 public class CodeTaskForgeProject extends Application {
 
 
-
-    
     public static void main(String[] args) {
         launch(args);
     }
@@ -31,7 +21,7 @@ public class CodeTaskForgeProject extends Application {
 
        // SQLConnection.all2you();
        Parent root;
-       root = FXMLLoader.load(getClass().getResource("/code/task/forge/project/Views/Datasheet/Datasheet.fxml"));
+       root = FXMLLoader.load(getClass().getResource("/code/task/forge/project/Views/MainMenu/MainMenu.fxml"));
        Scene scene = new Scene(root, 830,500);
 
        app_stage.setTitle("Code Task Force");
