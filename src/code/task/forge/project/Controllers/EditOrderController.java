128c35967c56e900f89153f78a7acba9f27a04d1
André Marques <andrepestanamarques@gmail.com>
2021-06-09 20:05:15 +0100

    Fix Order Controllers

--- src/code/task/forge/project/Controllers/EditOrderController.java
+++ src/code/task/forge/project/Controllers/EditOrderController.java
@@ -7,7 +7,11 @@ package code.task.forge.project.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +20,29 @@ import javafx.fxml.Initializable;
  */
 public class EditOrderController implements Initializable {
 
+    @FXML
+    private Button btnReturn;
+    @FXML
+    private Button btnEdit;
+    @FXML
+    private TextField txtNif1;
+    @FXML
+    private TextField txtAddressDelivery;
+    @FXML
+    private TextField txtAddressBill;
+    @FXML
+    private TextField txtDocDate;
+    @FXML
+    private TextField txtNumberDoc;
+    @FXML
+    private TextField txtDescount;
+    @FXML
+    private TextField txtStatus;
+    @FXML
+    private TextField txtTotal;
+    @FXML
+    private TextField txtLine;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +50,50 @@ public class EditOrderController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void returnOrderApp(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void EditClient(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtNif(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtAddressDelivery(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtAddressBill(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtDocDate(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtNumberDoc(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtDescount(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtStatus(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtTotal(ActionEvent event) {
+    }
+
+    @FXML
+    private void OrderTxtLine(ActionEvent event) {
+    }
     
 }
